dac82bb91939db61fd3e1b4505f61f3e4c3b6e4d
zeku87 <nocomment182@gmail.com>
2018-01-17 10:20:43 +0100

    gulp error handling implemented in the css-task

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -5,9 +5,11 @@ const cssvars = require('postcss-simple-vars');
 const cssnested = require('postcss-nested');
 const cssImport = require('postcss-import');
 
-
 gulp.task('css', () => {
 	return gulp.src('./app/assets/styles/styles.css')
 	.pipe(postcss([cssImport, autoprefixer, cssvars, cssnested]))
+	.on('error', (error) => {
+		console.error(error.toString());
+	})
 	.pipe(gulp.dest('./app/temp/styles'));
 });
